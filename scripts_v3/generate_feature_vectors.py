6148bfe7e1d9fff37351730012ba1d98549ae22d
mingzhang <mingzhangyang2015@gmail.com>
2024-04-05 19:01:50 -0400

    rename the output file

--- scripts_v3/generate_feature_vectors.py
+++ scripts_v3/generate_feature_vectors.py
@@ -38,6 +38,6 @@ df = pd.DataFrame({'CurName': data['CurName'], 'features': embeddings})
 print(df.head())
 
 # Save the DataFrame
-output_file = os.path.join(DATA_DIR, 'Dataframe_CurName_features_embedding_finetuned_biobert.pkl')
+output_file = os.path.join(DATA_DIR, 'Dataframe_CurName_features_embedding_finetuned_biomedbert.pkl')
 df.to_pickle(output_file)
 
